a28d8f9c50f032c4ebd7b7a8c96dad7568c3c466
BrLacerda <Brun0lac3rd4@gmail.com>
2022-10-28 04:22:00 -0300

    madruga

--- js/recuperarSenha.js
+++ js/recuperarSenha.js
@@ -1,3 +1,15 @@
+
 function redefSenha() {
-    document.getElementById(feedbackRedef).innerHTML(`<center>`)
+    const email = document.getElementById("email")
+    const feedback = document.getElementById("feedbackRedef")
+    
+    
+    if (email.value == "") {
+        const falhaMsg = feedback.innerHTML = `<center>Precisamos do seu email para recuperar sua conta!</center>`
+        return falhaMsg
+    }
+    else  {
+        const validadoMsg = feedback.innerHTML = `<center>Enviamos um email para você com um link para redefinir sua senha. Caso não tenha recebido, verifique se você digitou seu email corretamente</center>`
+        return validadoMsg
+    }
 }
